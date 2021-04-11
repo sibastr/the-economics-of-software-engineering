a37a5763368ec99d002668629cfc2f40115dc9da
sibastr <49680365+sibastr@users.noreply.github.com>
2021-04-12 02:20:10 +0300

    lab6_interface_upgrage
    Interface without budget

--- Lab6_EPE/lab6_var3.py
+++ Lab6_EPE/lab6_var3.py
@@ -1,6 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QMessageBox
+from prettytable import PrettyTable
+import sys
+from mainwindow import Ui_MainWindow
+
 eaf_table = {
     'RELY': [0.75, 0.86, 1.0, 1.15, 1.4],
     'DATA': [None, 0.94, 1.0, 1.08, 1.16],
@@ -40,6 +45,7 @@ def get_default_eaf_levels():
     return eaf_levels
 #Трудозатраты
 def labor_costs(c1: float, eaf: float, size: float, p1: float) -> float:
+    #print('c1',c1,' eaf',eaf,' size',size,' p1',p1)
     return c1 * eaf * (size)**p1
 #Время
 def time(c2: float, l_c: float, p2: float) -> float:
@@ -78,15 +84,16 @@ def cocomo(size: int, eaf_l: list, mode_func):
 
 eaf_levels = get_default_eaf_levels()
 
-def RelyImpact(size):
-    eaf_levels = get_default_eaf_levels()
+def RelyImpact(size,eaf_levels):
+    #eaf_levels = get_default_eaf_levels()
+    eaf_copy = eaf_levels.copy()
     RelyResult = []
     list_of_modes = [normal_mode, inter_mode, inbuilt_mode]
     for i in range(3):
         mode = list_of_modes[i]
         for j in range(5):
-            eaf_levels['RELY'] = j
-            temp_result = cocomo(size, eaf_levels, mode)
+            eaf_copy['RELY'] = j
+            temp_result = cocomo(size, eaf_copy, mode)
             RelyResult.append(temp_result)
     dict_result = {
         'RELY_Normal': RelyResult[0:5],
@@ -95,17 +102,18 @@ def RelyImpact(size):
     }
     return dict_result
 
-def DataImpact(size):
-    eaf_levels = get_default_eaf_levels()
+def DataImpact(size,eaf_levels):
+    #eaf_levels = get_default_eaf_levels()
+    eaf_copy = eaf_levels.copy()
     DataResult = []
     list_of_modes = [normal_mode, inter_mode, inbuilt_mode]
     for i in range(3):
         mode = list_of_modes[i]
         for j in range(1,5):
-            eaf_levels['DATA'] = j
+            eaf_copy['DATA'] = j
             #print(eaf_levels['DATA'])
 
-            temp_result = cocomo(size, eaf_levels, mode)
+            temp_result = cocomo(size, eaf_copy, mode)
             DataResult.append(temp_result)
     dict_result = {
         'DATA_Normal': DataResult[0:4],
@@ -113,15 +121,16 @@ def DataImpact(size):
         'DATA_Inbuilt': DataResult[8:12]
     }
     return dict_result
-def CPLXImpact(size):
-    eaf_levels = get_default_eaf_levels()
+def CPLXImpact(size,eaf_levels):
+    eaf_copy = eaf_levels.copy()
+    #eaf_levels = get_default_eaf_levels()
     CPLXResult = []
     list_of_modes = [normal_mode, inter_mode, inbuilt_mode]
     for i in range(3):
         mode = list_of_modes[i]
         for j in range(5):
-            eaf_levels['CPLX'] = j
-            temp_result = cocomo(size, eaf_levels, mode)
+            eaf_copy['CPLX'] = j
+            temp_result = cocomo(size, eaf_copy, mode)
             CPLXResult.append(temp_result)
     dict_result = {
         'CPLX_Normal' : CPLXResult[0:5],
@@ -184,55 +193,91 @@ def Impact_Graph(dict_RELY,dict_CPLX,dict_Data):
         time_Data_Inbuilt.append(dict_Data['DATA_Inbuilt'][i][1])
 
     # Work graph
-    plt.plot([0,1,2,3,4],work_RELY_Normal,[0,1,2,3,4],work_CPLX_Normal,[1,2,3,4],work_Data_Normal)
-
-    plt.ylabel('Work Normal')
-    plt.xlabel('Factors')
-    plt.legend(['RELY','CPLX','DATA'])
+    fig, axs = plt.subplots(2, 3)
+    axs[0, 0].plot([0,1,2,3,4],work_RELY_Normal,[0,1,2,3,4],work_CPLX_Normal,[1,2,3,4],work_Data_Normal)
+    axs[0, 0].set_title('Normal mode')
+    axs[0, 0].legend(['RELY', 'CPLX', 'DATA'])
+    axs[0, 1].plot([0, 1, 2, 3, 4], work_RELY_Inter, [0, 1, 2, 3, 4], work_CPLX_Inter, [1, 2, 3, 4], work_Data_Inter)
+    axs[0, 1].set_title('Inter mode')
+    axs[0, 1].legend(['RELY', 'CPLX', 'DATA'])
+    axs[0, 2].plot([0, 1, 2, 3, 4], work_RELY_Inbuilt, [0, 1, 2, 3, 4], work_CPLX_Inbuilt, [1, 2, 3, 4], work_Data_Inbuilt)
+    axs[0, 2].set_title('Inbuilt mode')
+    axs[0, 2].legend(['RELY', 'CPLX', 'DATA'])
+    axs[1, 0].plot([0, 1, 2, 3, 4], time_RELY_Normal, [0, 1, 2, 3, 4], time_CPLX_Normal, [1, 2, 3, 4], time_Data_Normal)
+    axs[1, 0].set_title('Normal mode')
+    axs[1, 0].legend(['RELY', 'CPLX', 'DATA'])
+    axs[1, 1].plot([0, 1, 2, 3, 4], time_RELY_Inter, [0, 1, 2, 3, 4], time_CPLX_Inter, [1, 2, 3, 4], time_Data_Inter)
+    axs[1, 1].set_title('Inter mode')
+    axs[1, 1].legend(['RELY', 'CPLX', 'DATA'])
+    axs[1, 2].plot([0, 1, 2, 3, 4], time_RELY_Inbuilt, [0, 1, 2, 3, 4], time_CPLX_Inbuilt, [1, 2, 3, 4], time_Data_Inbuilt)
+    axs[1, 2].set_title('Inbuilt mode')
+    axs[1, 2].legend(['RELY', 'CPLX', 'DATA'])
+    #for ax in axs.flat:
+    axs[0,0].set(xlabel='Уровень фактора', ylabel='Трудозатраты')
+    axs[0, 1].set(xlabel='Уровень фактора', ylabel='Трудозатраты')
+    axs[0, 2].set(xlabel='Уровень фактора', ylabel='Трудозатраты')
+    axs[1, 0].set(xlabel='Уровень фактора', ylabel='Время')
+    axs[1, 1].set(xlabel='Уровень фактора', ylabel='Время')
+    axs[1, 2].set(xlabel='Уровень фактора', ylabel='Время')
     plt.show()
 
-    plt.plot([0, 1, 2, 3, 4], work_RELY_Inter, [0, 1, 2, 3, 4], work_CPLX_Inter, [1, 2, 3, 4], work_Data_Inter)
-    plt.ylabel('Work Intermediate')
-    plt.xlabel('Factors')
-    plt.legend(['RELY', 'CPLX', 'DATA'])
-    plt.show()
+    #for ax in axs.flat:
+    #    ax.set(xlabel='x-label', ylabel='y-label')
+    #plt.plot([0,1,2,3,4],work_RELY_Normal,[0,1,2,3,4],work_CPLX_Normal,[1,2,3,4],work_Data_Normal)
 
-    plt.plot([0, 1, 2, 3, 4], work_RELY_Inbuilt, [0, 1, 2, 3, 4], work_CPLX_Inbuilt, [1, 2, 3, 4], work_Data_Inbuilt)
-    plt.ylabel('Work Inbuilt')
-    plt.xlabel('Factors')
-    plt.legend(['RELY', 'CPLX', 'DATA'])
+    #plt.ylabel('Work Normal')
+    #plt.xlabel('Factors')
+    #plt.legend(['RELY','CPLX','DATA'])
+    #plt.show()
 
-    plt.show()
+    #plt.plot([0, 1, 2, 3, 4], work_RELY_Inter, [0, 1, 2, 3, 4], work_CPLX_Inter, [1, 2, 3, 4], work_Data_Inter)
+    #plt.ylabel('Work Intermediate')
+    #plt.xlabel('Factors')
+    #plt.legend(['RELY', 'CPLX', 'DATA'])
+    #plt.show()
+
+    #plt.plot([0, 1, 2, 3, 4], work_RELY_Inbuilt, [0, 1, 2, 3, 4], work_CPLX_Inbuilt, [1, 2, 3, 4], work_Data_Inbuilt)
+    #plt.ylabel('Work Inbuilt')
+    #plt.xlabel('Factors')
+    #plt.legend(['RELY', 'CPLX', 'DATA'])
+
+    #plt.show()
     # Time graph
-    plt.plot([0, 1, 2, 3, 4], time_RELY_Normal, [0, 1, 2, 3, 4], time_CPLX_Normal, [1, 2, 3, 4], time_Data_Normal)
+    #plt.plot([0, 1, 2, 3, 4], time_RELY_Normal, [0, 1, 2, 3, 4], time_CPLX_Normal, [1, 2, 3, 4], time_Data_Normal)
 
-    plt.ylabel('Time Normal')
-    plt.xlabel('Factors')
-    plt.legend(['RELY', 'CPLX', 'DATA'])
-    plt.show()
+    #plt.ylabel('Time Normal')
+    #plt.xlabel('Factors')
+    #plt.legend(['RELY', 'CPLX', 'DATA'])
+    #plt.show()
 
-    plt.plot([0, 1, 2, 3, 4], time_RELY_Inter, [0, 1, 2, 3, 4], time_CPLX_Inter, [1, 2, 3, 4], time_Data_Inter)
-    plt.ylabel('Time Intermediate')
-    plt.xlabel('Factors')
-    plt.legend(['RELY', 'CPLX', 'DATA'])
-    plt.show()
+    #plt.plot([0, 1, 2, 3, 4], time_RELY_Inter, [0, 1, 2, 3, 4], time_CPLX_Inter, [1, 2, 3, 4], time_Data_Inter)
+    #plt.ylabel('Time Intermediate')
+    #plt.xlabel('Factors')
+    #plt.legend(['RELY', 'CPLX', 'DATA'])
+    #plt.show()
 
-    plt.plot([0, 1, 2, 3, 4], time_RELY_Inbuilt, [0, 1, 2, 3, 4], time_CPLX_Inbuilt, [1, 2, 3, 4], time_Data_Inbuilt)
-    plt.ylabel('Time Inbuilt')
-    plt.xlabel('Factors')
-    plt.legend(['RELY', 'CPLX', 'DATA'])
+    #plt.plot([0, 1, 2, 3, 4], time_RELY_Inbuilt, [0, 1, 2, 3, 4], time_CPLX_Inbuilt, [1, 2, 3, 4], time_Data_Inbuilt)
+    #plt.ylabel('Time Inbuilt')
+    #plt.xlabel('Factors')
+    #plt.legend(['RELY', 'CPLX', 'DATA'])
 
-    plt.show()
+    #plt.show()
 
-def project_tradion():
-    eaf_levels = get_default_eaf_levels()
-    eaf_levels['RELY'] = 4
-    eaf_levels['TIME'] = 4
-    eaf_levels['SCED'] = 4
-    eaf_levels['PCAP'] = 4
-    eaf_levels['ACAP'] = 4
-    eaf_levels['CPLX'] = 4
-    result_Air = cocomo(430000 / 1000, eaf_levels, inter_mode)
+def project_tradion(eaf,kloc,mode,self):
+    #print('AAAA')
+    eaf_levels = eaf.copy()
+    #kloc = 430
+    #eaf_levels['RELY'] = 3
+    #eaf_levels['TIME'] = 3
+    #eaf_levels['SCED'] = 3
+    #eaf_levels['PCAP'] = 3
+    #eaf_levels['ACAP'] = 3
+    #eaf_levels['CPLX'] = 3
+    #Example
+    #eaf_levels['TOOL'] = 3
+    #eaf_levels['AEXP'] = 3
+
+    result_Air = cocomo(kloc, eaf_levels,mode)
     amount_of_workers = []
 
     work_plan_requirements = result_Air[0] * 0.08
@@ -282,6 +327,56 @@ def project_tradion():
     print("Итоговая работа:", result_Air[0] + work_plan_requirements)
     print("Итоговое время:", result_Air[1] + time_plan_requirements)
     print("\n------------------------------------------------------------------------------\n")
+
+    # Переносим значения в UI
+    self.ui.work.setPlainText(str(round(result_Air[0],2)))
+    self.ui.time.setPlainText(str(round(result_Air[1],2)))
+    self.ui.work_plan.setPlainText(str(round(result_Air[0] + work_plan_requirements,2)))
+    self.ui.time_plan.setPlainText(str(round(result_Air[1] + time_plan_requirements,2)))
+
+    # Заполняем таблицу традиционных трудозатрат в UI
+    item = QTableWidgetItem()
+    item.setText(str(round(work_plan_requirements,2)))
+    self.ui.tableWidget_2.setItem(0, 0, item)
+
+    item = QTableWidgetItem()
+    item.setText(str(round(work_design_project,2)))
+    self.ui.tableWidget_2.setItem(1, 0, item)
+
+    item = QTableWidgetItem()
+    item.setText(str(round(work_details_design,2)))
+    self.ui.tableWidget_2.setItem(2, 0, item)
+
+    item = QTableWidgetItem()
+    item.setText(str(round(work_code_test,2)))
+    self.ui.tableWidget_2.setItem(3, 0, item)
+
+    item = QTableWidgetItem()
+    item.setText(str(round(work_integration_test,2)))
+    self.ui.tableWidget_2.setItem(4, 0, item)
+
+    # Заполняем таблицу традиционного времени в UI
+
+    item = QTableWidgetItem()
+    item.setText(str(round(time_plan_requirements,2)))
+    self.ui.tableWidget_2.setItem(0, 1, item)
+
+    item = QTableWidgetItem()
+    item.setText(str(round(time_design_project,2)))
+    self.ui.tableWidget_2.setItem(1, 1, item)
+
+    item = QTableWidgetItem()
+    item.setText(str(round(time_details_design,2)))
+    self.ui.tableWidget_2.setItem(2, 1, item)
+
+    item = QTableWidgetItem()
+    item.setText(str(round(time_code_test,2)))
+    self.ui.tableWidget_2.setItem(3, 1, item)
+
+    item = QTableWidgetItem()
+    item.setText(str(round(time_integration_test,2)))
+    self.ui.tableWidget_2.setItem(4, 1, item)
+
     result_Air[0] += work_plan_requirements
     result_Air[1] += time_plan_requirements
     analysis = result_Air[0] * 0.04
@@ -303,15 +398,59 @@ def project_tradion():
     print("Канцелярия проекта", office)
     print("Управление конфигурацией и обеспечение качества", quality_assurance)
     print("Создание руководств", manuals)
-
-
     print("Итоговые человеко-месяцы", summary_work)
-    #print(amount_of_workers)
-    #print(len(amount_of_workers))
-    #print(amount_of_workers.count(44))
-    #print(amount_of_workers.count(122))
-    #print(amount_of_workers.count(127))
-    #print(amount_of_workers.count(97))
+    # Заполняем таблицу трудозатрат WBS
+    item = QTableWidgetItem()
+    item.setText(str(round(analysis,2)))
+    self.ui.tableWidget.setItem(0, 0, item)
+
+    item = QTableWidgetItem()
+    item.setText(str(round(design_product, 2)))
+    self.ui.tableWidget.setItem(1, 0, item)
+
+    item = QTableWidgetItem()
+    item.setText(str(round(programming, 2)))
+    self.ui.tableWidget.setItem(2, 0, item)
+
+    item = QTableWidgetItem()
+    item.setText(str(round(testing, 2)))
+    self.ui.tableWidget.setItem(3, 0, item)
+
+    item = QTableWidgetItem()
+    item.setText(str(round(verification, 2)))
+    self.ui.tableWidget.setItem(4, 0, item)
+
+    item = QTableWidgetItem()
+    item.setText(str(round(office, 2)))
+    self.ui.tableWidget.setItem(5, 0, item)
+
+    item = QTableWidgetItem()
+    item.setText(str(round(quality_assurance, 2)))
+    self.ui.tableWidget.setItem(6, 0, item)
+
+    item = QTableWidgetItem()
+    item.setText(str(round(manuals, 2)))
+    self.ui.tableWidget.setItem(7, 0, item)
+
+    item = QTableWidgetItem()
+    item.setText(str(round(summary_work, 2)))
+    self.ui.tableWidget.setItem(8, 0, item)
+
+    #amount_of_workers += [math.ceil(work_integration_test / time_integration_test)] * round(
+    #   time_integration_test)
+    print(amount_of_workers)
+    print(len(amount_of_workers))
+    #print(amount_of_workers.count(10))
+    #print(amount_of_workers.count(22))
+    #print(amount_of_workers.count(60))
+    #print(amount_of_workers.count(62))
+    #print(amount_of_workers.count(48))
+    dif_workers = []
+    dif_workers.append(amount_of_workers[0])
+    for i in range(len(amount_of_workers)):
+        if amount_of_workers[i] not in dif_workers:
+            dif_workers.append(amount_of_workers[i])
+    print(dif_workers)
     x = np.arange(1, len(amount_of_workers)+1)
     y = amount_of_workers
 
@@ -322,27 +461,152 @@ def project_tradion():
     #ax.set_facecolor('seashell')
     #fig.set_facecolor('floralwhite')
     plt.xlabel('Месяцы')
-
-    plt.text(8, 21, 20)
-    plt.text(24, 45, 44)
-    plt.text(36, 123, 122)
-    plt.text(44, 128, 127)
-    plt.text(54, 98, 97)
-
+    """
+    plt.text(amount_of_workers.count(dif_workers[0])/2, dif_workers[0]+1, dif_workers[0])
+    plt.text(amount_of_workers.count(dif_workers[1])/2+amount_of_workers.count(dif_workers[0]),
+             dif_workers[1]+1,dif_workers[1])
+    plt.text(amount_of_workers.count(dif_workers[2])/2+
+             amount_of_workers.count(dif_workers[0]) +
+             amount_of_workers.count(dif_workers[1]), dif_workers[2]+1, dif_workers[2])
+    plt.text(amount_of_workers.count(dif_workers[3]) / 2 +
+             amount_of_workers.count(dif_workers[0]) +
+             amount_of_workers.count(dif_workers[1]) +
+             amount_of_workers.count(dif_workers[2]), dif_workers[3] + 1, dif_workers[3])
+    plt.text(amount_of_workers.count(dif_workers[4]) / 2 +
+             amount_of_workers.count(dif_workers[0]) +
+             amount_of_workers.count(dif_workers[1]) +
+             amount_of_workers.count(dif_workers[2]) +
+             amount_of_workers.count(dif_workers[3]), dif_workers[4] + 1, dif_workers[3])
+    """
     fig.set_figwidth(12)  # ширина Figure
     fig.set_figheight(6)  # высота Figure
     plt.ylabel('Количество сотрудников')
-
     plt.show()
+
+    
+
+
+class mywindow(QMainWindow):
+    def __init__(self):
+        super(mywindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+
+        #d_Rely = RelyImpact(15000/1000,eaf_levels)
+        #d_Data = DataImpact(15000/1000,eaf_levels)
+        #d_CPLX = CPLXImpact(15000/1000,eaf_levels)
+
+        self.ui.pushButton_calculate.clicked.connect(self.onTaskBtnClick)
+        self.ui.pushButton_graph.clicked.connect(self.onGraphBtnClick)
+        #Impact_Graph(d_Rely,d_CPLX,d_Data)
+    def addItemTableWidget(self, row, column, value):
+        item = QTableWidgetItem()
+        item.setText(str(value))
+        self.ui.tableWidget_2.setItem(row, column, item)
+        #item = QTableWidgetItem()
+        #item.setText(str(5))
+        #self.ui.tableWidget_2.setItem(0, 0, item)
+    def onTaskBtnClick(self):
+        eaf_levels = get_default_eaf_levels()
+        eaf_levels['RELY'] = self.ui.RELY.value()
+        eaf_levels['DATA'] = self.ui.DATA.value()
+        eaf_levels['CPLX'] = self.ui.CPLX.value()
+        eaf_levels['TIME'] = self.ui.TIME.value()
+        eaf_levels['STOR'] = self.ui.STOR.value()
+        eaf_levels['VIRT'] = self.ui.VIRT.value()
+        eaf_levels['TURN'] = self.ui.TURN.value()
+        eaf_levels['ACAP'] = self.ui.ACAP.value()
+        eaf_levels['AEXP'] = self.ui.AEXP.value()
+        eaf_levels['PCAP'] = self.ui.PCAP.value()
+        eaf_levels['VEXP'] = self.ui.VEXP.value()
+        eaf_levels['LEXP'] = self.ui.LEXP.value()
+        eaf_levels['MODP'] = self.ui.MODP.value()
+        eaf_levels['TOOL'] = self.ui.TOOL.value()
+        eaf_levels['SCED'] = self.ui.SCED.value()
+
+        loc = self.ui.LOC.value()
+        kloc = loc / 1000
+
+        if self.ui.normal.isChecked():
+            mode = normal_mode
+        elif self.ui.inter.isChecked():
+            mode = inter_mode
+        elif self.ui.inbuilt.isChecked():
+            mode = inbuilt_mode
+        #d_Rely = RelyImpact(15000/1000, eaf_levels)
+        #d_Data = DataImpact(15000/1000,eaf_levels)
+        #d_CPLX = CPLXImpact(15000/1000,eaf_levels)
+
+        #self.work.setPlainText("Hello PyQt5!\nfrom pythonpyqt.com")
+        project_tradion(eaf_levels,kloc,mode,self)
+        #self.addItemTableWidget(2, 0, 6 * '*')
+        #item = QTableWidgetItem()
+        #item.setText(str(2))
+        #self.ui.tableWidget.setItem(0, 0, item)
+        #print(item)
+        #item = QTableWidgetItem()
+        #item.setText(str(5))
+        #self.ui.tableWidget_2.setItem(0,0,item)
+        #self.ui.addItemTableWidget(0, 1, '1')
+        #item = QtWidgets.QTableWidgetItem()
+        #tableWidget.setItem(0, 0, QTableWidgetItem("Text in column 1"))
+    def addItemTableWidget(self, row, column, value):
+        item = QTableWidgetItem()
+        item.setText(str(value))
+        self.ui.tableWidget.setItem(row, column, item)
+
+
+    def onGraphBtnClick(self):
+        eaf_levels = get_default_eaf_levels()
+        eaf_levels['RELY'] = self.ui.RELY.value()
+        eaf_levels['DATA'] = self.ui.DATA.value()
+        eaf_levels['CPLX'] = self.ui.CPLX.value()
+        eaf_levels['TIME'] = self.ui.TIME.value()
+        eaf_levels['STOR'] = self.ui.STOR.value()
+        eaf_levels['VIRT'] = self.ui.VIRT.value()
+        eaf_levels['TURN'] = self.ui.TURN.value()
+        eaf_levels['ACAP'] = self.ui.ACAP.value()
+        eaf_levels['AEXP'] = self.ui.AEXP.value()
+        eaf_levels['PCAP'] = self.ui.PCAP.value()
+        eaf_levels['VEXP'] = self.ui.VEXP.value()
+        eaf_levels['LEXP'] = self.ui.LEXP.value()
+        eaf_levels['MODP'] = self.ui.MODP.value()
+        eaf_levels['TOOL'] = self.ui.TOOL.value()
+        eaf_levels['SCED'] = self.ui.SCED.value()
+
+        loc = self.ui.LOC.value()
+        kloc = loc/1000
+
+        #if self.ui.normal.isChecked():
+        #    mode = normal_mode
+        #elif self.ui.inter.isChecked():
+        #    mode = inter_mode
+        #elif self.ui.inbuilt.isChecked():
+        #    mode = inbuilt_mode
+        d_Rely = RelyImpact(kloc, eaf_levels)
+        d_Data = DataImpact(kloc, eaf_levels)
+        d_CPLX = CPLXImpact(kloc, eaf_levels)
+        Impact_Graph(d_Rely, d_CPLX, d_Data)
+
+
+
 
 
 if __name__ == '__main__':
-    d_Rely = RelyImpact(15000/1000)
-    d_Data = DataImpact(15000/1000)
-    d_CPLX = CPLXImpact(15000/1000)
-
+    #eaf_levels = get_default_eaf_levels()
+    #d_Rely = RelyImpact(15000/1000,eaf_levels)
+    #d_Data = DataImpact(15000/1000,eaf_levels)
+    #d_CPLX = CPLXImpact(15000/1000,eaf_levels)
+    #print(eaf_levels)
     #Impact_Graph(d_Rely,d_CPLX,d_Data)
-    project_tradion()
+    #project_tradion(eaf_levels,430000)
+    app = QApplication([])
+    application = mywindow()
+    application.show()
+
+    sys.exit(app.exec())
 
 
 
